26cf1419d3d8538b69470d19c3e2871f7374c883
rezamarbun0507 <rezamarbun0507>
2025-03-12 18:33:19 +0700

    PGWEBLLLL

---
+++ app/Models/PoligonsModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PoligonsModel extends Model
+{
+    protected $table = 'poligons';
+
+    protected $guarded= ['id'];
+}
---
+++ resources/views/table.blade.php
@@ -0,0 +1,28 @@
+@extends('layout/template')
+
+@section('content')
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Kelas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Reza Tristian Gilbran</td>
+                <td>23/515309/SV/22520</td>
+                <td>A</td>
+            </tr>
+            <tr></tr>
+            <td>2</td>
+            <td>Sohee</td>
+            <td>23/51233/SV/12442</td>
+            <td>A</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
